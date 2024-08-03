e5251465931721f990ae34a85ee2260d7d525b93
KruthikaP-21 <kruthikap.cs20@rvce.edu.in>
2024-08-03 20:42:48 +0530

    workflows add

--- app1/config.py
+++ app1/config.py
@@ -2,15 +2,21 @@ import os
 
 class Config:
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Krutu%402001@localhost/task_manager1'
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///test.db')
+    try:
+        SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    except:
+        SQLALCHEMY_DATABASE_URI = os.getenv('sqlite:///test.db')
     # SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)
     JWT_SECRET_KEY = os.urandom(24)
 
 class TestingConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Krutu%402001@localhost/task_manager1'
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///test.db')
+    try:
+        SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    except:
+        SQLALCHEMY_DATABASE_URI = os.getenv('sqlite:///test.db')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///test.db')
     TESTING = True
     JWT_SECRET_KEY = 'your_testing_secret_key'
